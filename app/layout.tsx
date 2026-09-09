597cb3fef0c569be4677d9c9c1a8dd50e42b9672
gabrielbalenton <gabrielbalenton@gmail.com>
2026-09-09 10:39:52 +0800

    Apply FPX 2026 brand system globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "../components/revised-pages.css";
 import "../components/inner-refresh.css";
+import "../components/brand-system-2026.css";
 
 const siteUrl = "https://fpx.nz";
 const socialImage = "/images/social/fpx-social-share.jpg";
