08813f0c72944fb7187e616046df2dd95e31abb5
gabrielbalenton <gabrielbalenton@gmail.com>
2026-09-11 15:47:16 +0800

    Load final design polish after existing page styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "../components/revised-pages.css";
 import "../components/inner-refresh.css";
 import "../components/brand-system-2026.css";
 import "../components/source-timber-redesign.css";
+import "../components/design-polish-2026.css";
 
 const siteUrl = "https://fpx.nz";
 const socialImage = "/images/social/fpx-social-share.jpg";
