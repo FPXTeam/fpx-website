56c4ef752775c9179f9f68ff24b5cf1170f4da41
gabrielbalenton <gabrielbalenton@gmail.com>
2026-09-21 09:24:12 +0800

    Tag Saw Point page subscriptions

--- components/saw-point-page.tsx
+++ components/saw-point-page.tsx
@@ -173,6 +173,7 @@ export function SawPointPage(){
 
       <form className="spx-form" aria-label="Saw Point subscription" onSubmit={subscribe}>
         <input className="fpx-honeypot" name="website" tabIndex={-1} autoComplete="off" aria-hidden="true"/>
+        <input type="hidden" name="source" value="Saw Point page"/>
         <div className="spx-form-icon"><Mail aria-hidden="true"/></div>
         <h3>Join the Saw Point list</h3>
         <label><span>Name</span><input name="name" autoComplete="name" placeholder="Your name" required/></label>
