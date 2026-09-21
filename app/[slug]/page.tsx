2105dea4bc852005fb474128234b66491875a089
gabrielbalenton <gabrielbalenton@gmail.com>
2026-09-21 16:26:18 +0800

    Scope revised page styles to inner routes

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { notFound, permanentRedirect } from "next/navigation";
 import { MasterInnerPage } from "@/components/master-site";
 import sawPointIssues from "../../data/saw-point-issues.json";
+import "../../components/revised-pages.css";
 import "../../components/inner-refresh.css";
 import "../../components/source-timber-redesign.css";
 import "../../components/premium-inner-pages.css";
